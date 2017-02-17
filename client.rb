a7982d03ba3b67e68cab15ba9ae2e667b8c61df9
Sumon SM <sumonsm00@gmail.com>
2017-02-17 07:02:10 -0500

    refactored to more OO style

--- client.rb
+++ client.rb
@@ -1,4 +1,5 @@
 class Client
+  include ClientCustomErrors
     
   # Initialize variables
   def initialize(hostname=nil, port=nil, bytes=nil)
@@ -75,24 +76,26 @@ class Client
   end
 end
 
-# Custom exception for empty payload
-class EmptyPayloadException < StandardError
-  def initialize(msg="Error: Payload is empty!")
-    super
+module ClientCustomErrors
+  # Custom exception for empty payload
+  class EmptyPayloadException < StandardError
+    def initialize(msg="Error: Payload is empty!")
+      super
+    end
   end
-end
 
-# Custom exception for wrong payload encoding
-class PayloadEncodingException < StandardError
-  def initialize(msg="Error: Payload is not encoded in UTF-8!")
-    super
+  # Custom exception for wrong payload encoding
+  class PayloadEncodingException < StandardError
+    def initialize(msg="Error: Payload is not encoded in UTF-8!")
+      super
+    end
   end
-end
 
-# Custom exception for wrong payload formatting
-class PayloadFormattingException < StandardError
-  def initialize(msg="Error: Payload is not formatted correctly!")
-    super
+  # Custom exception for wrong payload formatting
+  class PayloadFormattingException < StandardError
+    def initialize(msg="Error: Payload is not formatted correctly!")
+      super
+    end
   end
 end
 
